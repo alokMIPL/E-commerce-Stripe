1aeab0ba8771bf9faaaeedf6bdf26645e75b2462
alokMIPL <alok@madhulata.com>
2025-09-04 12:12:54 +0530

    code added

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -5,7 +5,7 @@ export default function AdminLayout({ children }) {
     <>
       <section className="py-5 sm:py-7 bg-blue-100">
         <div className="container max-w-screen-xl mx-auto px-4">
-          <h1 className="text-bold text-2xl">Admin Dashboard</h1>
+          <h1 className="text-bold text-2xl">Admin Dashboard Testing</h1>
         </div>
       </section>
 
